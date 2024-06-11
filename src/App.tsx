91e66e48ed44ceb7b6e0c6e70f5c182e5d4a228d
chloedalligna <raionbyjinn@gmail.com>
2024-06-11 19:39:51 -0300

    feat: add Header and Hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Form from "./components/Form";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
 
@@ -13,7 +15,12 @@ function App() {
         width: "100vw",
       }}>
 
+      <Header/>
+
+      <Hero/>
+
       <Form/>
+
     </div>
   );
 
